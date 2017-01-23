44e6a7ccbb2eac6e4e24e6f6b42e21a6a5e1fab8
NeoYin <neoyin@NeoYin-MacBook-Pro.local>
2017-01-23 11:24:08 +0800

    fix bug

--- src/main/java/com/lifeix/designer/UserVerify.java
+++ src/main/java/com/lifeix/designer/UserVerify.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by neoyin on 2017/1/13.
@@ -20,11 +21,11 @@ public class UserVerify {
     private RedisUtil redisUtil;
 
     public User verify(String userId,String deviceId)throws NotAuthorizedException {
-        if (StringUtils.isEmpty(userId)||StringUtils.isEmpty(deviceId)) throw new NotAuthorizedException("user id or device id is null");
+        if (StringUtils.isEmpty(userId)||StringUtils.isEmpty(deviceId)) throw new NotAuthorizedException("user id or device id is null", Response.status(Response.Status.UNAUTHORIZED));
         User user = (User) redisUtil.getObject(User.class,userId);
-        if (user==null)throw new NotAuthorizedException("not found this user");
+        if (user==null)throw new NotAuthorizedException("not found this user", Response.status(Response.Status.UNAUTHORIZED));
         Device online = user.getOnline();
-        if (online==null||online.getDeviceId()==null||!deviceId.equals(online.getDeviceId()))throw new NotAuthorizedException("this account login other deviceid");
+        if (online==null||online.getDeviceId()==null||!deviceId.equals(online.getDeviceId()))throw new NotAuthorizedException("this account login other deviceid", Response.status(Response.Status.UNAUTHORIZED));
         return user;
     }
 
