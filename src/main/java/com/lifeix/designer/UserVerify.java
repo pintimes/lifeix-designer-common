19329f03ec5abed3e5e210da79a5ff9c8309a6dc
NeoYin <neoyin@NeoYin-MacBook-Pro.local>
2017-01-25 08:54:44 +0800

    add log

--- src/main/java/com/lifeix/designer/UserVerify.java
+++ src/main/java/com/lifeix/designer/UserVerify.java
@@ -3,6 +3,8 @@ package com.lifeix.designer;
 import com.lifeix.designer.model.user.Device;
 import com.lifeix.designer.model.user.User;
 import com.lifeix.designer.utils.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -17,6 +19,9 @@ import javax.ws.rs.core.Response;
 @Component("userVerify")
 public class UserVerify {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(UserVerify.class);
+
+
     @Autowired
     private RedisUtil redisUtil;
 
@@ -25,7 +30,10 @@ public class UserVerify {
         User user = (User) redisUtil.getObject(User.class,userId);
         if (user==null)throw new NotAuthorizedException("not found this user", Response.status(Response.Status.UNAUTHORIZED));
         Device online = user.getOnline();
-        if (online==null||online.getDeviceId()==null||!deviceId.equals(online.getDeviceId()))throw new NotAuthorizedException("this account login other deviceid", Response.status(Response.Status.UNAUTHORIZED));
+        if (online==null||online.getDeviceId()==null||!deviceId.equals(online.getDeviceId())){
+            LOGGER.warn(" this account login other deviceid "+user);
+            throw new NotAuthorizedException("this account login other deviceid", Response.status(Response.Status.UNAUTHORIZED));
+        }
         return user;
     }
 
